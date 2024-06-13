8a6b64e7dcedbdb4159cbcd5893701e2ed6a5fba
Pete Hayes <pete@hayes.id.au>
2024-06-14 09:38:31 +1000

    Restore header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { AnimatedLogo } from '@/components/animated-logo';
 import { Container } from '@/components/container';
 import { SiteFooter } from '@/components/site-footer';
+import { SiteHeader } from '@/components/site-header';
 
 export default function Home() {
 	return (
 		<main className="flex w-full min-h-screen flex-col items-center justify-between">
+			<SiteHeader />
 			<Container>
 				<div className="flex flex-col gap-3">
 					<AnimatedLogo />
