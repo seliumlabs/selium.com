35f14ff383e126ecc4e6e40d1843fbd1e616f528
Pete Hayes <pete@hayes.id.au>
2024-06-14 22:14:35 +1000

    Google Analytics

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -1,3 +1,5 @@
+import { GoogleAnalytics } from '@next/third-parties/google';
+
 const date = new Date();
 const year = date.getFullYear();
 
@@ -5,6 +7,7 @@ export const SiteFooter = () => {
   return (
     <footer className="mt-16 py-4 text-neutral-600 text-center">
       <p className="text-xs">&copy; {year} Selium Labs</p>
+      <GoogleAnalytics gaId="G-0K0K8BGVDP" />
     </footer>
   );
 };
